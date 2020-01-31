c64939d96d9b656b439921c6924960364267e389
Caspar-Chen-hku <susume@connect.hku.hk>
2020-01-31 13:26:09 -0600

    Update main.cpp

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -16,8 +16,8 @@ int main(int argc, char** argv) {
   float (*c_base)[kJ] = nullptr;
 
   MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &numproc);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   clog << "\nrank: " << rank << ", numproc: " << numproc << "\n";
 
