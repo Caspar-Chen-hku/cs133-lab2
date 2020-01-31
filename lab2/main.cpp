d5d0c59ecbf59aa759cf54154493579945aa39b6
Caspar-Chen-hku <susume@connect.hku.hk>
2020-01-31 14:34:06 -0600

    Update main.cpp

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -32,20 +32,38 @@ int main(int argc, char** argv) {
     GemmBaseline(a, b, c_base);
 
     clog << "\nRun parallel GEMM with MPI\n";
+  }
+
+    float (*a_buffer)[kK] = nullptr;
+    float (*b_buffer)[kJ] = nullptr;
+    float (*c_buffer)[kJ] = nullptr;
+
+    for (int i=0; i<kI/4; i++){
+      a_buffer[i] = std:aligned_alloc(32, kK);
+    }
+
+    for (int i=0; i<kK; i++){
+      b_buffer[i] = std:aligned_alloc(32, kJ);
+    }
+
+    for (int i=0; i<kI/4; i++){
+      c_buffer[i] = std:aligned_alloc(32, kJ);
+    }
 
     int aCount = kI*kK/numproc;
-    MPI_Scatter(a, aCount, MPI_FLOAT, a, aCount, MPI_FLOAT, kRoot, MPI_COMM_WORLD);
-  }
+    int bCount = kK*kJ;
+    MPI_Scatter(a, aCount, MPI_FLOAT, a_buffer, aCount, MPI_FLOAT, kRoot, MPI_COMM_WORLD);
+    MPI_Scatter(b, bCount, MPI_FLOAT, b_buffer, bCount, MPI_FLOAT, kRoot, MPI_COMM_WORLD);
 
   MPI_Barrier(MPI_COMM_WORLD);
   double begin = MPI_Wtime();
-  GemmParallelBlocked(a, b, c);
+  GemmParallelBlocked(a_buffer, b_buffer, c_buffer);
   MPI_Barrier(MPI_COMM_WORLD);
   double end = MPI_Wtime();
 
+  int cCount = kI*kJ/numproc;
+  MPI_Gather(c, cCount, MPI_FLOAT, c_buffer, cCount, MPI_FLOAT, kRoot, MPI_COMM_WORLD);
   if (rank == kRoot) {
-    int cCount = kI*kJ/numproc;
-    MPI_Gather(c, cCount, MPI_FLOAT, c, cCount, MPI_FLOAT, kRoot, MPI_COMM_WORLD);
 
     double run_time = end - begin;
     float gflops = 2.0 * kI * kJ * kK / (run_time * 1e9);
@@ -53,7 +71,7 @@ int main(int argc, char** argv) {
     clog << "Perf: " << gflops << " GFlops\n";
 
     bool fail = false;
-    if (Diff(c_base, c) != 0) {
+    if (Diff(c_base, c_buffer) != 0) {
       fail = true;
     }
 
