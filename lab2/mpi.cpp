02dd4d90eb5f0b2848c87cb4f8ac428823d81067
Caspar-Chen-hku <susume@connect.hku.hk>
2020-02-01 18:54:12 -0600

    Update mpi.cpp

--- lab2/mpi.cpp
+++ lab2/mpi.cpp
@@ -182,23 +182,23 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
   }
 */
 
-index_a = 0;
-for (int i=0; i< kI/numproc; i++){
-  if (rank==0){
-    std::memset(c[i], 0, sizeof(float) * kJ);
-  } 
-        //index_a = i*kK;
+  index_a = 0;
+  for (int i=0; i< kI/numproc; i++){
+    if (rank==0){
+      std::memset(c[i], 0, sizeof(float) * kJ);
+    } 
         for (int k=0; k< kK; k++){
           index_b = k*kJ;
+          index_c = i*kJ;
           for (int j=0; j< kJ; j++)
           {
-            //index_b += j;
               if (rank==0){
                 c[i][j] += a[i][k] * b[k][j];
               }else{
-                c_buffer[i*kJ+j] += a_buffer[index_a]*b_buffer[index_b];
+                c_buffer[index_c] += a_buffer[index_a]*b_buffer[index_b];
               }
               index_b++;
+              index_c++;
           }
           index_a++;
         }
