3669526becb45040a4e6527fb91c4f1a2f7034fe
Caspar-Chen-hku <susume@connect.hku.hk>
2020-02-03 16:25:20 -0600

    Update mpi.cpp

--- lab2/mpi.cpp
+++ lab2/mpi.cpp
@@ -177,18 +177,18 @@ MPI_Request request;
     for (int i=0; i< kI/numproc; i+=BLOCK_SIZE_I){
       for (int k=0; k< kK; k+=BLOCK_SIZE_K){
         for (int j=0; j< kJ; j+=BLOCK_SIZE_J){
-          index_a = 0;
       for (int i0=i; i0<i+BLOCK_SIZE_I; i0++){
+        index_a = i0*kJ+k;
         for (int k0=k; k0<k+BLOCK_SIZE_K; k0++){
-          index_b = k0*kJ;
-          index_c = i0*kJ;
+          index_b = k0*kJ+j;
+          index_c = i0*kJ+j;
           for (int j0=j; j0<j+BLOCK_SIZE_J; j0++){
             
                   if (rank==0){
                     c[i0][j0] += a[i0][k0] * b[k0][j0];
                   }else{
                     //c_buffer[i0*kJ+j0] += a_buffer[i0*kJ+k0] * b_buffer[k0*kJ+j0];
-                    c_buffer[index_c] += a_buffer[i0*kJ+k0] * b_buffer[index_b];
+                    c_buffer[index_c] += a_buffer[index_a] * b_buffer[index_b];
                     index_b++;
                     index_c++;
                   }
