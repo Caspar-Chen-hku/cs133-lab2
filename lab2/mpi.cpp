ed9ec28bcc9ca41d7487328d8ec941b018e61e63
Caspar-Chen-hku <susume@connect.hku.hk>
2020-02-03 16:36:47 -0600

    Update mpi.cpp

--- lab2/mpi.cpp
+++ lab2/mpi.cpp
@@ -188,7 +188,7 @@ MPI_Request request;
 */
 /***********************CALCULATE*************************/
   
-/*
+
   int BLOCK_SIZE_I = kI/8;
   int BLOCK_SIZE_K = kK/128;
   int BLOCK_SIZE_J = kJ/2;
@@ -219,49 +219,7 @@ MPI_Request request;
       }
       }
   }
-*/
 
-int BLOCK_SIZE_I = kI/8;
-  int BLOCK_SIZE_K = kK/128;
-  int BLOCK_SIZE_J = kJ/2;
-
-if (rank == 0) {
-  for (int i=0; i< kI/numproc; i+=BLOCK_SIZE_I){
-      for (int k=0; k< kK; k+=BLOCK_SIZE_K){
-        for (int j=0; j< kJ; j+=BLOCK_SIZE_J){
-      for (int i0=i; i0<i+BLOCK_SIZE_I; i0++){
-        for (int k0=k; k0<k+BLOCK_SIZE_K; k0++){
-          for (int j0=j; j0<j+BLOCK_SIZE_J; j0++){
-                    c[i0][j0] += a[i0][k0] * b[k0][j0];
-            }
-          }
-        }
-      }
-      }
-  }
-}else{
-  int index_a, index_b, index_c;
-
-    for (int i=0; i< kI/numproc; i+=BLOCK_SIZE_I){
-      for (int k=0; k< kK; k+=BLOCK_SIZE_K){
-        for (int j=0; j< kJ; j+=BLOCK_SIZE_J){
-      for (int i0=i; i0<i+BLOCK_SIZE_I; i0++){
-        index_a = i0*kJ+k;
-        for (int k0=k; k0<k+BLOCK_SIZE_K; k0++){
-          index_b = k0*kJ+j;
-          index_c = i0*kJ+j;
-          for (int j0=j; j0<j+BLOCK_SIZE_J; j0++){
-                    c_buffer[index_c] += a_buffer[index_a] * b_buffer[index_b];
-                    index_b++;
-                    index_c++;
-            }
-            index_a++;
-          }
-        }
-      }
-      }
-  }
-}
 
 
 /*
