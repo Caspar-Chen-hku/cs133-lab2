6f60228ee651f11533b59608cc3ee14f55c5646e
Caspar-Chen-hku <susume@connect.hku.hk>
2020-02-03 13:34:23 -0600

    Update mpi.cpp

--- lab2/mpi.cpp
+++ lab2/mpi.cpp
@@ -191,18 +191,33 @@ MPI_Request request;
   int BLOCK_SIZE_J = 1024;
   int BLOCK_SIZE_K = 8;
   float temp;
+  int index_c;
 
     for (int i=0; i< kI; i+=BLOCK_SIZE_I){
         for (int k=0; k< kK; k+=BLOCK_SIZE_K){
           for (int j=0; j< kJ; j+=BLOCK_SIZE_J){
             for (int i0=i; i0<i+BLOCK_SIZE_I; i0++){
               for (int j0=j; j0<j+BLOCK_SIZE_J; j0++){
-                temp = c[i0][j0];
+                if (rank==0){
+                  temp = c[i0][j0];
+                }else{
+                  index_c = i0*kJ+j0;
+                  temp = c_buffer[index_c];
+                }
                 for (int k0=k; k0<k+BLOCK_SIZE_K; k0++){
                   //c[i0][j0] += a[i0][k0] * b[k0][j0];
-                  temp += a[i0][k0] * b[k0][j0];
+                  if (rank==0){
+                    temp += a[i0][k0] * b[k0][j0];
+                  }else{
+                    temp += a_buffer[i0*kK+k0] * b_buffer[k0*kJ+j0];
+                  }
+                  
                 }
-                c[i0][j0] = temp;
+                if(rank==0){
+                  c[i0][j0] = temp;
+                }else{
+                  c_buffer[index_c] = temp;
+                }
               }
             }
           }
