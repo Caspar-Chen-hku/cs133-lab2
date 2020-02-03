dac6b02389d2b9c78a20f0f79e725592e27568b9
Caspar-Chen-hku <susume@connect.hku.hk>
2020-02-03 13:24:43 -0600

    Update mpi.cpp

--- lab2/mpi.cpp
+++ lab2/mpi.cpp
@@ -189,7 +189,7 @@ MPI_Request request;
 
   int BLOCK_SIZE_I = kI/8;
   int BLOCK_SIZE_K = kK/128;
-  int BLOCK_SIZE_J = kJ/4;
+  int BLOCK_SIZE_J = kJ/2;
 
     for (int i=0; i< kI/numproc; i+=BLOCK_SIZE_I){
       for (int k=0; k< kK; k+=BLOCK_SIZE_K){
