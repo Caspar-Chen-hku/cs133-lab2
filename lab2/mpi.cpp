3b8deb90603cd135418aa78b06e76804a245481d
Caspar-Chen-hku <susume@connect.hku.hk>
2020-02-04 13:43:57 -0600

    Update mpi.cpp

--- lab2/mpi.cpp
+++ lab2/mpi.cpp
@@ -148,31 +148,27 @@ int BLOCK_SIZE_I = 64;
     for (int i=0; i< kI; i+=BLOCK_SIZE_I){
         for (int k=0; k< kK; k+=BLOCK_SIZE_K){
           for (int j=0; j< kJ; j+=BLOCK_SIZE_J){
-            for (int i0=i; i0<i+BLOCK_SIZE_I; i0++){
-              for (int j0=j; j0<j+BLOCK_SIZE_J; j0++){
                 if (rank == 0){
-                  temp = c[i0][j0];
+                  temp = c[i][j];
                 }else{
-                  temp = c_buffer[i0*kJ+j0];
+                  temp = c_buffer[i*kJ+j];
                 }
                 
                 for (int k0=k; k0<k+BLOCK_SIZE_K; k0++){
                   //c[i0][j0] += a[i0][k0] * b[k0][j0];
                   if (rank == 0){
-                    temp += a[i0][k0] * b[k0][j0];
+                    temp += a[i][k0] * b[k0][j];
                   }else{
-                    temp += a_buffer[i0*kJ+k0] * b_buffer[k0*kJ+j0];
+                    temp += a_buffer[i*kJ+k0] * b_buffer[k0*kJ+j];
                   }
                   
                 }
                 if (rank == 0){
-                  c[i0][j0] = temp;
+                  c[i][j] = temp;
                 }else{
-                  c_buffer[i0*kJ+j0] = temp;
+                  c_buffer[i*kJ+j] = temp;
                 }
                 
-              }
-            }
           }
         }
   }
