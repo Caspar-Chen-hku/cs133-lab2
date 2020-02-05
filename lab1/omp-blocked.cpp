11b3b2a44af46e5c6ee9cb1f60c2c18258bb6617
Caspar-Chen-hku <susume@connect.hku.hk>
2020-02-05 10:29:18 -0600

    Update omp-blocked.cpp

--- lab1/omp-blocked.cpp
+++ lab1/omp-blocked.cpp
@@ -10,13 +10,15 @@
 void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
                          float c[kI][kJ]) {
   // Your code goes here...
-  //int BLOCK_SIZE_I = kI/8;
-  //int BLOCK_SIZE_J = kJ/4;
-  //int BLOCK_SIZE_K = kK/64;
+  int BLOCK_SIZE_I = kI/8;
+  int BLOCK_SIZE_J = kJ/4;
+  int BLOCK_SIZE_K = kK/64;
 
+  /*
   int BLOCK_SIZE_I = 64;
   int BLOCK_SIZE_J = 1024;
   int BLOCK_SIZE_K = 8;
+  */
 
   #pragma omp parallel for schedule(static) num_threads(8)
     for (int i=0; i< kI; i+=BLOCK_SIZE_I){
@@ -32,13 +34,13 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
           for (int j=0; j< kJ; j+=BLOCK_SIZE_J){
             for (int i0=i; i0<i+BLOCK_SIZE_I; i0++){
               //std::memset(c[i0], 0, sizeof(float) * kJ);
-              for (int j0=j; j0<j+BLOCK_SIZE_J; j0++){
-                float temp = c[i0][j0];
-                for (int k0=k; k0<k+BLOCK_SIZE_K; k0++){
-                  //c[i0][j0] += a[i0][k0] * b[k0][j0];
-                  temp += a[i0][k0] * b[k0][j0]; //a_buffer[i0-i][k0-k] * b[k0][j0];
+              for (int k0=k; k0<k+BLOCK_SIZE_K; k0++){
+                //float temp = c[i0][j0];
+                for (int j0=j; j0<j+BLOCK_SIZE_J; j0++){
+                  c[i0][j0] += a[i0][k0] * b[k0][j0];
+                  //temp += a[i0][k0] * b[k0][j0]; //a_buffer[i0-i][k0-k] * b[k0][j0];
                 }
-                c[i0][j0] = temp;
+                //c[i0][j0] = temp;
               }
             }
           }
