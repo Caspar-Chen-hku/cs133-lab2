4dbdfd7f2622a71c137be12dbdd65efda95b30e5
Caspar-Chen-hku <susume@connect.hku.hk>
2020-02-05 15:06:49 -0600

    Update mpi.cpp

--- lab2/mpi.cpp
+++ lab2/mpi.cpp
@@ -31,48 +31,14 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
   int bCount = kK*kJ;
   int cCount = kI*kJ/numproc;
 
-  /*
-  int half_size = kI/2;
-  int count = half_size*half_size;
-  */
-
-
   float *a_buffer;
   float *b_buffer;
   float *c_buffer;
   
-  //if (rank != 0){
-    a_buffer = (float*) std::aligned_alloc(64, aCount*sizeof *a_buffer);
-    b_buffer = (float*) std::aligned_alloc(64, bCount*sizeof *b_buffer);
-    c_buffer = (float*) std::aligned_alloc(64, cCount*sizeof *c_buffer);
-    std::memset(c_buffer, 0, sizeof(float) * cCount);
-  //}
-
-/*
-  if (rank == 0){
-    for (int i=0; i<kI/numproc; i++){
-      for (int k=0; k<kK; k++){
-        a_buffer[i*kK+k] = a[i][k];
-      }
-    }
-    for (int i=0; i<kK; i++){
-      for (int k=0; k<kJ; k++){
-        b_buffer[i*kJ+k] = b[i][k];
-      }
-    }
-  }
-  */
-/*
- float (*a_buffer)[kK] = nullptr;
- float (*b_buffer)[kJ] = nullptr;
-
-  if (rank != 0){
-    a_buffer = new float[kI/numproc][kK];
-    b_buffer = new float[kK][kJ];
-    c = new float[kI/numproc][kJ];
-  }
-*/
- //float c_buffer[kI/numproc][kJ];
+  a_buffer = (float*) std::aligned_alloc(64, aCount*sizeof *a_buffer);
+  b_buffer = (float*) std::aligned_alloc(64, bCount*sizeof *b_buffer);
+  c_buffer = (float*) std::aligned_alloc(64, cCount*sizeof *c_buffer);
+  std::memset(c_buffer, 0, sizeof(float) * cCount);
 
   int rows = kI/numproc;
   int offset = rows;
@@ -81,25 +47,18 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
 
   /**************SEND BLOCKS OF DATA*******************/
 
-
   if (rank == 0){
   memcpy(b_buffer, b, sizeof(float)*bCount);
     for (int i=1; i<numproc; i++){
-      //MPI_Send(&a[offset][0], aCount, MPI_FLOAT, i, 1,
-      //             MPI_COMM_WORLD);
       MPI_Send(b, bCount, MPI_FLOAT, i, 2, MPI_COMM_WORLD);
       offset += rows;
     }
   }else{
-    //MPI_Recv(a_buffer, aCount, MPI_FLOAT, 0, 1, MPI_COMM_WORLD, &status);
     MPI_Recv(b_buffer, bCount, MPI_FLOAT, 0, 2, MPI_COMM_WORLD, &status);
   }
 
-//clog << "sent\n";
-
 MPI_Scatter(a, aCount, MPI_FLOAT, a_buffer,
     aCount, MPI_FLOAT, 0,  MPI_COMM_WORLD);
-
 
 /*
 MPI_Request request;
@@ -161,39 +120,21 @@ MPI_Request request;
 */
 /***********************CALCULATE*************************/
 
-
-  //int BLOCK_SIZE_I = 32;
-  //int BLOCK_SIZE_K = 8;
-  //int BLOCK_SIZE_J = kJ/32;
   int BLOCK_SIZE_I = 16;
   int BLOCK_SIZE_K = 8;
   int BLOCK_SIZE_J = 16;
   float temp;
-  //float* a_temp = (float*) std::aligned_alloc(64, BLOCK_SIZE_I*BLOCK_SIZE_K*sizeof *a_buffer);;
-  //int index_a, index_b, index_c;
 
     for (int i=0; i< kI/numproc; i+=BLOCK_SIZE_I){
       for (int k=0; k< kK; k+=BLOCK_SIZE_K){
         for (int j=0; j< kJ; j+=BLOCK_SIZE_J){
       for (int i0=i; i0<i+BLOCK_SIZE_I; i0++){
-        //index_a = i0*kJ+k;
         for (int j0=j; j0<j+BLOCK_SIZE_J; j0++){
-          //index_b = k0*kJ+j;
-          //index_c = i0*kJ+j;
           temp = 0;
           for (int k0=k; k0<k+BLOCK_SIZE_K; k0++){
-            
-                  //if (rank==0){
-                  //  c[i0][j0] += a_buffer[index_a] * b_buffer[index_b];
-                  //}else{
-                  temp += a_buffer[i0*kJ+k0] * b_buffer[k0*kJ+j0];;
-                    //c_buffer[i0*kJ+j0] += a_buffer[i0*kJ+k0] * b_buffer[k0*kJ+j0];
-                    //index_c++;
-                  //}
-                  //index_b++;
-            }
-            c_buffer[i0*kJ+j0] += temp;
-            //index_a++;
+              temp += a_buffer[i0*kJ+k0] * b_buffer[k0*kJ+j0];
+          }
+          c_buffer[i0*kJ+j0] += temp;
           }
         }
       }
