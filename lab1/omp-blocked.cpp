8c08f3acb2af7c63e75982e388dc98b07c854b97
Caspar-Chen-hku <susume@connect.hku.hk>
2020-02-05 10:16:45 -0600

    Update omp-blocked.cpp

--- lab1/omp-blocked.cpp
+++ lab1/omp-blocked.cpp
@@ -33,12 +33,12 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
             for (int i0=i; i0<i+BLOCK_SIZE_I; i0++){
               std::memset(c[i0], 0, sizeof(float) * kJ);
               for (int j0=j; j0<j+BLOCK_SIZE_J; j0++){
-                float temp = c[i0][j0];
+                float temp = 0.0; //c[i0][j0];
                 for (int k0=k; k0<k+BLOCK_SIZE_K; k0++){
                   //c[i0][j0] += a[i0][k0] * b[k0][j0];
                   temp += a[i0][k0] * b[k0][j0];
                 }
-                c[i0][j0] = temp;
+                c[i0][j0] += temp;
               }
             }
           }
