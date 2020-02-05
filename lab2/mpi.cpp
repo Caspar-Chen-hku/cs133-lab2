cf2a5e2aa8c0797e1fc8a0f1d9f75f3f9d5d0e36
Caspar-Chen-hku <susume@connect.hku.hk>
2020-02-04 20:06:57 -0600

    Update mpi.cpp

--- lab2/mpi.cpp
+++ lab2/mpi.cpp
@@ -77,7 +77,7 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
     MPI_Recv(b_buffer, bCount, MPI_FLOAT, 0, 2, MPI_COMM_WORLD, &status);
   }
 
-  clog << "sent b\n"
+  clog << "sent b\n";
 MPI_Scatter(a, aCount, MPI_FLOAT, a_buffer,
     aCount, MPI_FLOAT, 0,  MPI_COMM_WORLD);
 
